121362cb968be3c81168e599ff5b9b6c64f30217
HuynhLongNhat <nhathuynh227@gmail.com>
2024-08-20 09:53:10 +0700

    Halo Community app

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,10 @@
+import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+export default function Page() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <SignIn appearance={{ baseTheme: dark }} />;
+    </div>
+  )
+}
