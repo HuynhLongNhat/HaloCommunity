2950e5ad993dfe4f2e25743b3ddc0f568943d27d
HuynhLongNhat <nhathuynh227@gmail.com>
2024-08-23 14:28:46 +0700

    config middleware cors

--- middleware.js
+++ middleware.js
@@ -1,15 +1,4 @@
-// import { authMiddleware } from "@clerk/nextjs";
 
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.
-// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-// export default authMiddleware({
-//     publicRoutes: ["/api/webhook"]
-// });
-
-// export const config = {
-//     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 // Make sure that the `/api/webhooks/(.*)` route is not protected here
@@ -22,4 +11,27 @@ export const config = {
         // Always run for API routes
         '/(api|trpc)(.*)',
     ],
+}
+
+import { NextResponse } from "next/server";
+
+export function middleware() {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // add the CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
+}
+
+// specify the path regex to apply the middleware to
+export const config2 = {
+    matcher: '/api/:path*',
 }
